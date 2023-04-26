f2421bab10cedafd233325ef85c1c374613e8f6e
rajeh-dalbah <rajeh.dalbah@gmail.com>
2023-04-26 18:11:11 +0000

    lab 12

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,12 @@
-import Header from "../header/Header";
 import Tours from "../tours/Tours";
 import Footer from "../footer/Footer";
+import data from "../../data/db.json";
+import './Home.css';
 function Home(){
     return(
     <>
-        <Header/>
-        <Tours/>
+        
+        <Tours data={data}/>
         <Footer/>
     </>
     )
---
+++ src/components/tours/tour/tour.js
@@ -0,0 +1,15 @@
+import './tour.css'
+function Tour(props) {
+  
+    
+    return (
+        <>
+            <div >
+                <img src={props.tour.image} alt={props.tour.name} />
+                <h3>Name : {props.tour.name}</h3>
+            </div>
+        </>
+    );
+}
+
+export default Tour;
